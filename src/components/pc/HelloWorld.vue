eabc06024767352374d595eb0d325841a83b8c01
justone <645865839@qq.com>
2020-04-24 10:10:30 +0800

    git some push

--- src/components/pc/HelloWorld.vue
+++ src/components/pc/HelloWorld.vue
@@ -6,6 +6,11 @@
         <span @click="link_to_soical">粤ICP备19054851号-1</span>
       </p>
     </div>
+    <div class="blub">
+      <span :class="{action:to_play}"></span>
+      <img :src="music_src" alt @click="plays" />
+    </div>
+    <audio :src="mus" ref="themusic"></audio>
   </div>
 </template>
 
@@ -14,12 +19,32 @@ export default {
   name: "HelloWorld",
   data() {
     return {
-      msg: "Welcome to PC"
+      to_play: false,
+      music_src: require("../../assets/img/bulb.svg"),
+      mus: require("../../assets/qing.mp3")
     };
   },
-  methods:{
-    link_to_soical(){
-      window.open('http://beian.miit.gov.cn/');
+  methods: {
+    //备案网址
+    link_to_soical() {
+      window.open("http://beian.miit.gov.cn/");
+    },
+    //播放音乐  更换灯泡样式
+    plays() {
+      let but = this.$refs.themusic;
+      this.to_play = !this.to_play;
+      if (this.to_play) {
+        but.play();
+        this.music_src = require("../../assets/img/actb.svg");
+        this.$message({
+          type: "success",
+          message: "音乐开始播放！"
+        });
+      } else {
+        but.pause();
+        this.music_src = require("../../assets/img/bulb.svg");
+        this.$message("音乐停止播放！");
+      }
     }
   }
 };
@@ -27,19 +52,43 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang='scss' scoped>
+.action {
+  background-color: red !important;
+  height: 200px !important;
+  transition: 2s;
+}
 .hello {
   width: 100%;
   height: 100%;
   background: url("../../assets/img/8524.jpg") no-repeat;
   background-size: 100% 100%;
   position: relative;
+  .blub {
+    position: absolute;
+    right: 60px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    img {
+      width: 35px;
+      transform: rotate(180deg);
+      cursor: pointer;
+    }
+    span {
+      display: inline-block;
+      height: 120px;
+      width: 2px;
+      background-color: #bfbfbf;
+      transition: 2s;
+    }
+  }
   .website_links {
     position: absolute;
     bottom: 0%;
     left: 0;
     right: 0;
     text-align: center;
-    margin:0 auto;
+    margin: 0 auto;
     p {
       margin: 20px 0;
       color: gray;
